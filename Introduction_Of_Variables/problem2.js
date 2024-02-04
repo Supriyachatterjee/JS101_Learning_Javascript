ec900849911266a83e6a442dfb5f9b324a9e57f9
Supriya Chatterjee <roys04525@gmail.com>
2024-02-04 19:33:10 +0530

    Introduction to JS

---
+++ Introduction_Of_Variables/problem2.js
@@ -0,0 +1,6 @@
+var name = "Supriya Chatterjee"
+console.log("My Name; "+name)
+name = "Rantu Chatterjee"
+console.log("Fathers Name - "+name)
+name = "Aparna Chatterjee"
+console.log("Mothers Name - "+name)
---
+++ Introduction_Of_Variables/problem3.js
@@ -0,0 +1,6 @@
+var name = "Supriya Chatterjee"
+var age = "22"
+console.log("Name - "+name)
+console.log(typeof(name))
+console.log("Age - "+age)
+console.log(typeof(age))
